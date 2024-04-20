2e667a4034b147f65c947286f0fb8310abaad093
alexstr-dev <strmusic@protonmail.com>
2024-04-19 19:05:59 -0700

    Updated to 2.0 with config support now

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,14 @@ fn main() {
 
 ///Printing system info. Commands make me want to rip my hair out
 fn print_information() {
-    let system_info =
-        info::get_system_information().expect("Something went wrong. Please try again later!");
-    let packages = CommandUtils::get_command_output("pacman -Q | wc -l");
+    let to_print = replace_keys_and_return();
+    println!("{to_print}");
+}
+
+///Replacing keys in the config file and returning to print
+fn replace_keys_and_return() -> String {
+    let system_info = info::get_system_information().expect("Something went wrong. Please try again later!");
+    let output = CommandUtils::get_command_output("cat ~/.config/basefetch/config");
     let username = system_info.username;
     let hostname = system_info.hostname;
     let distro = system_info.distro_name;
@@ -20,20 +25,21 @@ fn print_information() {
     let uptime = system_info.uptime_formatted;
     let used_mem = system_info.used_mem;
     let total_mem = system_info.total_mem;
+    let available_mem = system_info.available_mem;
     let wm = std::env::var("XDG_CURRENT_DESKTOP").expect("Failed to get WM!");
     let cpu = CommandUtils::get_command_output("cpuid -1 | rg 'brand =' | cut -d '\"' -f2");
-    let gpu = CommandUtils::get_command_output(
-        "lspci -v -m | rg VGA -A 7 | rg Device | sed -n '2 p' | cut -d '[' -f2 | cut -d ']' -f1",
-    );
-    println!("BaseFetch\n___________________");
-    println!("{username}@{hostname}");
-    println!("Distro: {distro}");
-    println!("Kernel: {kernel}");
-    println!("Packages: {packages}");
-    println!("Shell: {shell}");
-    println!("WM: {wm}");
-    println!("CPU: {cpu}");
-    println!("GPU: {gpu}");
-    println!("Uptime: {uptime}");
-    println!("Memory: {used_mem}/{total_mem}");
+    let gpu = CommandUtils::get_command_output("lspci -v -m | rg VGA -A 7 | rg Device | sed -n '1 p' | cut -d '[' -f2 | cut -d ']' -f1");
+    let fuck_off = output.replace("{username}", &username)
+    .replace("{distro}", &distro)
+    .replace("{hostname}", &hostname)
+    .replace("{kernel}", &kernel)
+    .replace("{shell}", &shell)
+    .replace("{uptime}", &uptime)
+    .replace("{wm}", &wm)
+    .replace("{used_mem}", &used_mem)
+    .replace("{total_mem}", &total_mem)
+    .replace("{available_mem}", &available_mem)
+    .replace("{cpu}", &cpu)
+    .replace("{gpu}", &gpu);
+    return fuck_off;
 }
